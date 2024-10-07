e1a7ca5bb9b715bd09720242a4b04a8743167259
Tayo Ojo <tayoojo2001@gmail.com>
2024-10-07 08:24:37 -0400

    Add Mobile Nav

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -13,10 +13,10 @@ export function MainNav() {
             <Icons.logo className="h-6 w-6" />
             <span className="font-bold">{siteConfig.name}</span>
         </Link>
-        <Link href="/blog" className={cn("text-sm font-medium transition-colors hover:text-primary", pathname === "/blog" ? "text-foreground" : "text-foreground/60")}>
+        <Link href="/blog" className={cn("text-sm font-medium transition-colors hover:text-primary hidden sm:inline-block", pathname === "/blog" ? "text-foreground" : "text-foreground/60")}>
         Blog
         </Link>
-        <Link href="/about" className={cn("text-sm font-medium transition-colors hover:text-primary", pathname === "/about" ? "text-foreground" : "text-foreground/60")}>
+        <Link href="/about" className={cn("text-sm font-medium transition-colors hover:text-primary hidden sm:inline-block", pathname === "/about" ? "text-foreground" : "text-foreground/60")}>
         About
         </Link>
     </nav>;
--- components/site-header.tsx
+++ components/site-header.tsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 import { buttonVariants } from "./ui/button";
 import { Icons } from "./icons";
 import { MainNav } from "./main-nav";
+import { MobileNav } from "./mobile-nav";
 
 export function SiteHeader() {
     return (<header className= "sticky top-0 w-full border-b border-border bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
@@ -17,7 +18,7 @@ export function SiteHeader() {
                             rel="noreferrer"
                         >
                             <div 
-                            className={cn(buttonVariants({variant: "ghost"}), "w-10 px-0")}
+                            className={cn(buttonVariants({variant: "ghost"}), "w-10 px-0 hidden sm:inline-flex")}
                             >
                                 <Icons.github className="h-4 w-4" />
                                 <span className="sr-only">GitHub</span>
@@ -29,12 +30,13 @@ export function SiteHeader() {
                             rel="noreferrer"
                         >
                             <div 
-                            className={cn(buttonVariants({variant: "ghost"}), "w-10 px-0")}
+                            className={cn(buttonVariants({variant: "ghost"}), "w-10 px-0 hidden sm:inline-flex")}
                             >
                                 <Icons.linkedIn className="h-4 w-4" />
                                 <span className="sr-only">LinkedIn</span>
                             </div>
                         </Link>
+                        <MobileNav />
                     </nav>
                 </div>
             </div>
